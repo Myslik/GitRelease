938c14cf614baa8d65a33e3b90ba7fdf27d8e98d
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2017-06-09 22:32:13 +0200

    Remove Badger project

--- src/Badger/Metric.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Badger
-{
-    public class Metric
-    {
-        public string Name { get; set; }
-        public string Value { get; set; }
-        public string Color { get; set; }
-    }
-}
